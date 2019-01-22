3397b7473e9edbfa228334539bf91bf74a88c19c
rupengfei <a773849069@gmail.com>
2019-01-22 11:02:30 +0800

    this is test1

---
+++ RPF_Play.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+# ==========================================
+#       author: Pengfei.Ru
+#         mail: a773849069@gmail.com
+#         time: 2019/1/22
+# ==========================================
+import json
+import os
+
+# --*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*
+
+
+print("I'm coming")
+
+with open("D:/Repo/hello_world.py", "w") as f:
+    f.write("hello world")
+
+print("I'm leaving")
